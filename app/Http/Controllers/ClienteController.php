dd8c36873745747d1238ee38aa30b60b6e046dbd
ivaniglesias1897 <55801862+ivaniglesias1897@users.noreply.github.com>
2025-08-14 20:44:43 -0300

    Public store

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ClienteController extends Controller
 {
@@ -27,12 +28,35 @@ class ClienteController extends Controller
         //dd($ciudades); para ver lo que llega
 
         return view('clientes.create')->with('ciudades', $ciudades)
-                                      ->with('departamentos', $departamentos);
-        
+            ->with('departamentos', $departamentos);
     }
     public function store(Request $request)
     {
-        //
+        $input = $request->all();
+        $validation = Validator::make(
+            $input,
+            [
+                'clie_nombre.required',
+                'clie_apellido.required',
+                'clie_ci.required |unique:clientes,clie_ci',
+                'clie_fecha_nac.required| date',
+                'id_departamento.required|exists:departamentos,id_departamento',
+                'id_ciudad.required|exists:ciudades,id_ciudad',
+            ],
+            [
+                'clie_nombre.required' => 'Campo nombre es obligatorio',
+                'clie_apellido.required' => 'Campo apellido es obligatorio',
+                'clie_ci.required' => 'Campo CI es obligatorio',
+                'clie_ci.unique' => 'El dato CI ya existe',
+                'clie_fecha_nac.required' => 'Campo fecha de nacimiento es obligatoria',
+                'clie_fecha_nac.date' => 'La fecha de nacimiento debe ser una fecha válida',
+                'id_departamento.required' => 'Campo departamento es obligatorio',
+                'id_departamento.exists' => 'El campo departamento debe ser un departamento válido',
+                'id_ciudad.required' => 'Campo ciudad es obligatoria',
+                'id_ciudad.exists' => 'El campo ciudad debe ser una ciudad válida',
+
+            ]
+        );
     }
     public function edit($id)
     {
